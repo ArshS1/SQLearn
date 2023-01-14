0726f6bfd3a93d7878e82744b6504a8479438782
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2023-01-13 23:35:33 -0500

    fix list object issue

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 # database management
 
 def main():
+    # setup main page
     st.title("SQLearn")
     menu = ["Home", "About"]
     choice = st.sidebar.selectbox("Menu", menu)
@@ -13,7 +14,20 @@ def main():
         st.subheader("HomePage")
 
         # setup the layout
+        columnOne, columnTwo = st.columns(2)
         
+        print(type(columnOne))
+        with columnOne:
+                rawFormat = st.text_area("Enter SQL Command")
+                runCommand = st.form("Execute")
+
+        # Results
+        with columnTwo:
+            if runCommand:
+                st.info("Query Executed")
+                # allow user to copy the query
+                st.code(rawFormat)
+
     else:
         st.subheader("About")
 
