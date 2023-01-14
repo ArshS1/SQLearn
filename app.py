a8c924ee575bd8ea56a712dd9ff4c522d30ef76a
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2023-01-14 14:46:19 -0500

    finish query testing

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ import sqlite3
 connection = sqlite3.connect("data/data.sqlite")
 cursor = connection.cursor()
 
+# execute commands 
+def run_sqlCommands(raw: str) -> str:
+    cursor.execute(raw)
+    output = cursor.fetchall()
+    return output
+
+# original table info
+user_details = ["user_id", "username", "first_name", "last_name", "gender", "password", "status"]
+
 def main():
     # setup main page
     st.title("SQLearn")
@@ -21,15 +30,27 @@ def main():
         
         print(type(columnOne))
         with columnOne:
+            with st.form(key="Query Form"):
                 rawFormat = st.text_area("Enter SQL Command")
-                runCommand = st.form("Execute")
+                runCommand = st.form_submit_button("Execute")
+            with st.expander("Original Table Details"):
+                tableInfo = {"Dataset Name: user_details": user_details}
+                st.json(tableInfo)
 
         # Results of the command
         with columnTwo:
             if runCommand:
-                st.info("Query Executed")
+                st.info("Query Executed")  
                 # allow user to copy the query
                 st.code(rawFormat)
+
+                queryOutput = run_sqlCommands(rawFormat)
+                with st.expander("Output"):
+                    st.write(queryOutput)
+
+                with st.expander("Clean Table"):
+                    convertOutput = pd.DataFrame(queryOutput)
+                    st.dataframe(convertOutput)
 
     else:
         st.subheader("About")
